206934d61cb2a47a3446669032f4cc196717f6ae
maglili <gibson870811@gmail.com>
2021-02-25 17:27:12 +0800

    add rule

--- scripts-labeling_pos_data/labeling_gene.py
+++ scripts-labeling_pos_data/labeling_gene.py
@@ -1,5 +1,6 @@
 import pickle
 import pandas as pd
+import csv
 
 with open('../scripts_find_targets_alias/pickles/reverse_data_dict_biogrid.pickle', 'rb') as handle:
     reverse_data_dict_biogrid = pickle.load(handle)
@@ -7,45 +8,55 @@ with open('../scripts_find_targets_alias/pickles/reverse_data_dict_biogrid.pickl
 df = pd.read_csv('../csv_data/output_pos.csv', header=None, encoding='utf-8')
 df = df[[0,2]]
 
-pmid, text_1 = df.iloc[0]
-print(text_1)
-print()
+with open('labeling.tsv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile, delimiter='\t')
 
-for key,value in reverse_data_dict_biogrid.items():
-    if ' ' in key:
-        if key in text_1:
-            start_position = text_1.find(key)
-            end_position = start_position + len(key)
-            print('article_pmid:',pmid)
-            print('entity_text:',key)
-            print('entity_fbid:',reverse_data_dict_biogrid[key])
-            print('start_position:',start_position)
-            print('end_position:',end_position)
-            if text_1[start_position:end_position] != key:
-                print('Position Error!')
-                quit()
-            print('-'*20)
-    else:
-        continue
+    for idx in range(len(df)):
+        pmid, text_1 = df.iloc[idx]
+        text_1 = ' '.join(text_1.split())
+        #print(text_1)
 
-text_1_split = text_1.split()
-start_position = 0
-end_position = 0
-for word in text_1_split:
-    end_position = start_position + len(word)
-    if word in reverse_data_dict_biogrid:
-        print('article_pmid:',pmid)
-        print('entity_text:',word)
-        print('entity_fbid:',reverse_data_dict_biogrid[word])
-        print('start_position:',start_position)
-        print('end_position:',end_position)
-        if text_1[start_position:end_position] != word:
-            print('Position Error!')
-            quit()
-        print('-'*20)
-    start_position = end_position + 1
+        writer.writerow([text_1])
+        writer.writerow(['index', 'article_pmid','entity_text','entity_fbid','start_position','end_position'])
 
-print()
-# for key,value in reverse_data_dict_biogrid.items():
-#     if ' ' in key:
-#         print('{:25}   {:25}'.format(key,value))
+        # rule 1: scan the alias that comtaining white space.
+        # e.g. T beta h,  Dm Rg3
+        save_range=[]
+        for key,value in reverse_data_dict_biogrid.items():
+            if ' ' in key:
+                if key in text_1:
+                    start_position = text_1.find(key)
+                    end_position = start_position + len(key)
+                    if text_1[start_position:end_position] != key:
+                        print('Position Error!')
+                        quit()
+                    #print('-'*20)
+                    save_range.append(range(start_position, end_position))
+                    writer.writerow([idx, pmid, key, reverse_data_dict_biogrid[key], start_position, end_position])
+            else:
+                continue
+
+        # rule 2: scan the alias that only 1 word.
+        # e.g. olfD, sbl, bss
+        text_1_split = text_1.split()
+        start_position = 0
+        end_position = 0
+        for word in text_1_split:
+            skip_frag = False
+            end_position = start_position + len(word)
+            for range_ in save_range:
+                if start_position in range_: # Guarantee alias won't be add redundantly
+                    skip_frag = True
+                    continue
+            if word in reverse_data_dict_biogrid and skip_frag == False:
+                if text_1[start_position:end_position] != word:
+                    print('Position Error!')
+                    quit()
+                #print('-'*20)
+                writer.writerow([idx, pmid, word,reverse_data_dict_biogrid[word], start_position, end_position])
+            start_position = end_position + 1
+
+
+        writer.writerow([])
+        writer.writerow(['-'*20])
+        writer.writerow([])
